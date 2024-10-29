2ae83671ba58f897d6266d868365aea783e461f4
bassell01 <basselmohsen041@gmail.com>
2024-10-29 03:04:19 +0300

    adding header

---
+++ php/includes/header.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#"><span class="text-danger">MKA</span>NK</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="FindPage.php">Find</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">New projects</a></li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Explore</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Communities</a></li>
+                           
+                        </ul>
+                    </li>
+                </ul>
+                <button class="btn success">Log in</button>
+            </div>
+        </div>
+    </nav>
